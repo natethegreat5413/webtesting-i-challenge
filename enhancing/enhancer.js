84731e4443908bad107fd680b0faa68e45f02b0c
Nathan Cowley <nathancowley12@gmail.com>
2020-09-16 16:45:49 -0600

    ready to throw this computer

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,26 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancement < 20){
+    return {...item, enhancement: item.enhancement + 1}
+  }else{
+    return item
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){
+    return {...item, durability: item.durability - 5};
+  } else if (item.enhancement >= 15){
+    return {...item, durability: item.durability - 10}
+  } else if (item.enhancement > 16){
+    return {...item, enhancement: item.enhancement - 1}
+  }
+  // return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,38 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("runs enhancement tests", () => {
+        it("repair()", () => {
+            const item = {
+                name: 'pencil',
+                durability: 3,
+                enhancement: 10
+            } 
+            const newItem = enhancer.repair(item)
+            expect(newItem.durability).toBe(100)
+        })
+
+        it('should add 1 to enhancement if less than 20', () => {
+            expect(enhancer.success({
+                name: 'pencil',
+                durability: 3,
+                enhancement: 14,
+            })).toEqual({
+                name: 'pencil',
+                durability: 3,
+                enhancement: 15
+            })
+        })
+
+        it('should reduce durability by 5 if enhancement is < 15', () => {
+            const item = {
+                name: 'bike',
+                durability: 14,
+                enhancement: 12
+            }
+
+            const newItem = enhancer.fail(item)
+            expect(newItem.durability).toBe(9)
+        })
+    })
+
